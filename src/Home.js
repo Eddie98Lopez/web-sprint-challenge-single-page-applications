e68b2fc03498173b4e6db7bf1bf2ae197c950182
Eduardo Lopez <ed98lopez@gmail.com>
2020-12-12 09:58:57 -0800

    added a form and home coponent in a browserRouter, along with routes and links, and started setting up the form component

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+import * as Yup from 'yup'
+
+
+
+const Home = () => {
+
+  
+
+  return (
+    <div>
+
+    
+
+    
+    </div>
+  );
+};
+export default Home;
