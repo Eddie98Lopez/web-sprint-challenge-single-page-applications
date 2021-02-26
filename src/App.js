93549e126c6c5d1539c539c1a715e8df7f35a1c9
Eduardo Lopez <ed98lopez@gmail.com>
2021-02-26 10:59:48 -0800

    added the route paths, links, switch, and nav. Also started setting up the correct state in app to pass down to the form and cart components later. Added small styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import * as Yup from 'yup'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './Home'
+import Cart from './Cart'
+import Form from './Form'
+import formSchema from './formSchema'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+display: flex;
+justify-content:space-around;
+align-items: center;
+color:white;
+background: rgb(73,79,82);
+height:10vh;
+
+& a{
+  text-decoration:none;
+  color: white;
+  padding: 1rem
+}
+`
 
 const App = () => {
+
+  const initialCreds = {orderName: "", size:"", sauce:"", subsitute: "", specialInfo: "",toppings:{}}
+  const initialErrs = {orderName: "", size:"", sauce:"", subsitute: "", specialInfo: "",toppings:""}
+
+  const [formData, setFormData] = useState(initialCreds)
+  const [errors,setErrors]=useState(initialErrs)
+  const [disabled,setDisabled]=useState(true)
+  const [cart,setCart]= useState([])
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+
+      <Router>
+        <Nav>
+          <Link to='/'>Home</Link>
+          <Link to='/pizza'>Order Now</Link>
+          <Link to='/cart'>Cart</Link>
+        </Nav>
+
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+
+          <Route exact path='/pizza'>
+            <Form/>
+          </Route>
+
+          <Route exact path='/cart'>
+            <Cart/>
+          </Route>
+
+        </Switch>
+
+      </Router>
+
     </>
   );
 };
---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Route,Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Header = styled.header`
+
+width:100%;
+height: 70vh;
+background: grey;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+
+`
+
+const Home = (props)=>{
+    return(
+        <div>
+            <Header>
+                <h1>Order your pizza Now!!</h1>
+                <Link to='/pizza'><button>Order Now</button></Link>
+            </Header>
+        </div>
+    )
+}
+
+export default Home
